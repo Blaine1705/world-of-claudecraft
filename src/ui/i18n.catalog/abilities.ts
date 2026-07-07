9407e35db3f7aecc52de5d8ae8b8a44aaa1a8e0c
ryan-foo <ryan_mh@hotmail.com>
2026-07-07 17:17:59 +0800

    style: biome format for the codex i18n sweep

--- src/ui/i18n.catalog/abilities.ts
+++ src/ui/i18n.catalog/abilities.ts
@@ -1309,11 +1309,7 @@ const classAbilityNamesEn = {
         'Steel Cyclone',
         'Become a storm of steel, striking nearby enemies each second for {damage}. (Warrior talent)',
       ],
-      [
-        'blink',
-        'Flickerstep',
-        'Teleports you 15 yd forward and breaks roots. (Mage talent)',
-      ],
+      ['blink', 'Flickerstep', 'Teleports you 15 yd forward and breaks roots. (Mage talent)'],
       [
         'bloodlust',
         'War Drums',
@@ -1401,11 +1397,7 @@ const classAbilityNamesEn = {
         'Tolling Hammer',
         'Hurls a holy hammer at a wounded enemy for {damage} Holy damage. Only usable below 20% health. (Paladin talent)',
       ],
-      [
-        'healing_stream',
-        'Springwell',
-        'Restores a friendly target over 12 sec. (Shaman talent)',
-      ],
+      ['healing_stream', 'Springwell', 'Restores a friendly target over 12 sec. (Shaman talent)'],
       [
         'heroic_leap',
         'Warleap',
@@ -1427,11 +1419,7 @@ const classAbilityNamesEn = {
         'Encases you in ice, absorbing a massive amount of damage for 8 sec. (Mage talent)',
       ],
       ['inner_focus', 'Stilled Mind', 'Makes your next spell free. Lasts 60 sec. (Priest talent)'],
-      [
-        'innervate',
-        'Lifesap',
-        'Instantly restores 200 of your current resource. (Druid talent)',
-      ],
+      ['innervate', 'Lifesap', 'Instantly restores 200 of your current resource. (Druid talent)'],
       [
         'kick',
         'Boot',
